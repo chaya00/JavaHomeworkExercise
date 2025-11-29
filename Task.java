ab156caa457e00418a13576b2aa102f8cb92545a
Chaya <362chaya@gmail.com>
2025-11-29 22:22:28 +0200

    add Task class and Enum - status

--- Task.java
+++ Task.java
@@ -1,7 +1,44 @@
-
 public class Task {
-    public int Id;
-    public String Title;
-    public String Description;
-    public Status status;
+
+    private static int counter = 1;
+
+    private final int id;
+    private String title;
+    private String description;
+    private Status status;
+
+    public Task(String title, String description, Status status) {
+        this.id = counter++;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
