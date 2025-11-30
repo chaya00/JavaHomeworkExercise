ffdfbceb91fdaefbdaaa83573dfcfdcacc3c1046
Chaya <362chaya@gmail.com>
2025-11-30 20:50:26 +0200

    Added Word document with answers

--- Design/src/BorrowRequestDTO.java
+++
@@ -1,6 +0,0 @@
-public class BorrowRequestDTO {
-    private int bookId;
-    private String borrowerName;
-    private boolean isReturn ;
-
-}
---
+++ Design/src/LoanDTO.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.time.LocalDate;
+import java.util.*;
+public class LoanDTO {
+    private int id;
+    private int userId;
+    private int bookId;
+    private Boolean returned;
+    private LocalDate loanDate;
+    private LocalDate returnDate;
+
+}
